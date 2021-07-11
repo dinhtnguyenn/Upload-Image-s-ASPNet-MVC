22eeadec835309dfde26811112f292bce995c2af
Dinh Nguyen <32100818+dinhtnguyenn@users.noreply.github.com>
2021-07-11 16:08:07 +0700

    Initial commit

---
+++ BookManager/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using BookManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadImage(HttpPostedFileBase file)
+        {
+            if (file != null || file.ContentLength > 0)
+            {
+                string _FileName = Path.GetFileName(file.FileName);
+
+                string path = Path.Combine(Server.MapPath("/images/"), _FileName);
+                if (System.IO.File.Exists(path)) 
+                {
+                    //nếu hình ảnh đã tồn tại, thì xóa ảnh cũ, cập nhật lại ảnh mới
+                    System.IO.File.Delete(path);
+                    file.SaveAs(path);
+                }
+                else
+                {
+                    file.SaveAs(path);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult UploadMultipleImage(HttpPostedFileBase[] files)
+        {
+            foreach (HttpPostedFileBase file in files)
+            {
+                if (file != null || file.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+
+                    string path = Path.Combine(Server.MapPath("/images/"), _FileName);
+                    if (System.IO.File.Exists(path))
+                    {
+                        //nếu hình ảnh đã tồn tại, thì xóa ảnh cũ, cập nhật lại ảnh mới
+                        System.IO.File.Delete(path);
+                        file.SaveAs(path);
+                    }
+                    else
+                    {
+                        file.SaveAs(path);
+                    }
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BookManager/Models/Book.cs
@@ -0,0 +1,39 @@
+﻿namespace BookManager.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Book")]
+    public partial class Book
+    {
+        //[DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        [Required(ErrorMessage = "ID không được để trống")]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Tiêu đề không được để trống")]
+        [StringLength(100, ErrorMessage = "Tiêu đề không quá 100 ký tự")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Tên không được để trống")]
+        [StringLength(30, ErrorMessage = "Tên tác giả không quá 30 ký tự")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "Nội dung không được để trống")]
+        [StringLength(255)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Hình ảnh không được để trống")]
+        [StringLength(255)]
+        public string Images { get; set; }
+
+        [Required(ErrorMessage = "Giá không được để trống")]
+        [Range(1000, 1000000, ErrorMessage = "Giá sách từ 1000 đến 1000000")]
+        [Display(Name = "Giá sách")]
+        public int Price { get; set; }
+    }
+}
